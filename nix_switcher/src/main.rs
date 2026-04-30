2493c8d84cbfca1b534b6e74b144624441ee980d
TheGoatPrime234 <cato.jenisch@gmail.com>
2026-04-30 08:52:26 +0200

    New Fns

--- nix_switcher/src/main.rs
+++ nix_switcher/src/main.rs
@@ -58,16 +58,29 @@ fn gen_path(option: u8) -> String {
     }
 }
 
+fn gen_file_init() {
+    gen_file_config();
+    gen_file_links();
+    gen_file_wallpaper();
+}
+
 fn gen_file_config() {
     let themedir = format!("{}/themes", gen_path(2));
     let wallpaperdir = format!("{}/wallpaper", gen_path(2));
-    let mut theme_map: HashMap<String, Wallpapers> = HashMap::new();
     let basic_conf = Data {
         theme: String::from("dracula"),
         themedir: themedir,
         wallpaper: String::from("dracula"),
         wallpaperdir: wallpaperdir,
     };
+    let json_string = serde_json::to_string_pretty(&basic_conf).unwrap();
+    let json_path: String = format!("{}/config.json", gen_path(3));
+    fs::write(&json_path, &json_string).expect("Konnte Datei nicht schreiben");
+    println!("New File in: {}", &json_path);
+}
+
+fn gen_file_links() {
+    let mut theme_map: HashMap<String, Wallpapers> = HashMap::new();
     for i in pars_themes() {
         theme_map.insert(
             i,
@@ -79,14 +92,18 @@ fn gen_file_config() {
     let config_out = Config {
         theme: theme_map,
     };
-    let json_string = serde_json::to_string_pretty(&basic_conf).unwrap();
-    let json_path: String = format!("{}/config.json", gen_path(3));
-    fs::write(&json_path, &json_string).expect("Konnte Datei nicht schreiben");
-    println!("New File in: {}", &json_path);
     let json_string_wallpaper = serde_json::to_string_pretty(&config_out).unwrap();
     let json_path_wallpaper: String = format!("{}/links.json", gen_path(3));
     fs::write(&json_path_wallpaper, &json_string_wallpaper).expect("Konnte Datei nicht schreiben");
     println!("New File in: {}", &json_path_wallpaper);
+}
+
+
+fn gen_file_wallpaper() {
+    let json_string = serde_json::to_string_pretty(&pars_wall()).unwrap();
+    let json_path: String = format!("{}/wallpaper.json", gen_path(3));
+    fs::write(&json_path, &json_string).expect("Konnte Datei nicht schreiben");
+    println!("New File in: {}", &json_path);
 }
 
 fn change(theme: &str, wallpaper_index: usize) {
@@ -238,7 +255,7 @@ fn main() {
            apply(config);
        }
        Commands::Init => {
-           gen_file_config();
+           gen_file_init();
            println!("Generated Basic Config");
        }
     }
