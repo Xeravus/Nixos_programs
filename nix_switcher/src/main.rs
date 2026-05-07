b73a6b8bbcf2d0d8e9f8783664f457744ff0d505
TheGoatPrime234 <cato.jenisch@gmail.com>
2026-05-07 09:13:09 +0200

    Added Wallpaperfile to reload

--- nix_switcher/src/main.rs
+++ nix_switcher/src/main.rs
@@ -166,7 +166,7 @@ fn main() {
         Commands::Reload => {
             cl_themedir();
             gen_themes_all();
-            gen_file_init_wallpaper();
+            gen_file_wallpaper();
         }
         Commands::Genthemes => {
             gen_themes_all();
