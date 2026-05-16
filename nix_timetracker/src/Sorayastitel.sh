ff9e2ee91b1039e2b95c2079b2b637fbe348a90c
Xeravus <cato.jenisch@gmail.com>
2026-05-16 22:51:41 +0200

    Aktualisieren von Sorayastitel.sh

--- nix_timetracker/src/Sorayastitel.sh
+++ nix_timetracker/src/Sorayastitel.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 
-for soraya in super schön geenial die_größte schlauste beste perfekteste von_der_ananas_gesandte von_jesus_gesant von_der_ananas_gesegnet heilige geweite; do
+for soraya in super so_schön super_geenial die_größte die_schlauste die_beste die_perfekteste von_der_ananas_gesandte von_jesus_gesant von_der_ananas_gesegnet heilige geweite; do
     echo "Soraya ist $soraya"
 done
