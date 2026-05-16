1d64bdd9c5feb4bbb1efcd9b597fe7358a48971b
TheGoatPrime234 <cato.jenisch@gmail.com>
2026-05-16 23:00:40 +0200

    Entfernt

--- nix_timetracker/src/Sorayastitel.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env bash
-
-for soraya in super so_schön super_geenial die_größte die_schlauste die_beste die_perfekteste von_der_ananas_gesandte von_jesus_gesante von_der_ananas_gesegnet heilige geweite; do
-    echo "Soraya ist $soraya"
-done
