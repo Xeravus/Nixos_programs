bce1756c4c0c5aa36df5c69f1578dfa05a707d53
Xeravus <cato.jenisch@gmail.com>
2026-05-24 17:13:00 +0200

    Added Get Hardware Function

--- xanterella/src/main.rs
+++ xanterella/src/main.rs
@@ -7,6 +7,8 @@ use std::path;
 use std::collections::HashMap;
 use notify_rust::Notification;
 use clap::{Parser, Subcommand};
+use log::{debug, info, error};
+use env_logger::init;
 use serde_json::*;
 use inquire::Select;
 
@@ -16,6 +18,8 @@ use inquire::Select;
 pub struct Cli {
     #[command(subcommand)]
     command: Commands,
+    #[arg(long, global = true)]
+    pub debug: bool,
 }
 
 #[derive(Subcommand)]
@@ -29,6 +33,8 @@ pub enum Commands {
         tailfetch: bool,
         #[arg(short, long)]
         selecthost: bool,
+        #[arg(short, long)]
+        gethardware: bool,
     },
     RemoteInstall,
 }
@@ -49,6 +55,17 @@ pub struct DeviceInfo {
 
 pub fn main() {
     let cli = Cli::parse();
+    let log_level = if cli.debug {
+        log::LevelFilter::Debug
+    } else {
+        log::LevelFilter::Info
+    };
+    env_logger::builder()
+        .filter_level(log_level)
+        .format_target(false)
+        .format_timestamp(None)
+        .format_level(false)
+        .init();
     match &cli.command {
         Commands::Hostname => {
             let _ = Command::new("hostname") .spawn();
@@ -56,54 +73,63 @@ pub fn main() {
         Commands::Ping { ip } => {
             ssh_ping(ip);
         },
-        Commands::Debug { tailfetch, selecthost } => {
+        Commands::Debug { tailfetch, selecthost, gethardware } => {
             if *tailfetch {
-                println!("{:?}", tailscale_fetch());
+                debug!("{:?}", tailscale_fetch());
             } else if *selecthost {
-                select_host(tailscale_fetch());
+                debug!("{}", select_host(tailscale_fetch()));
+            } else if *gethardware {
+                ssh_get_hardware(&String::from("127.0.0.1"));
             } else {
-                println!("No Args")
+                debug!("No Args")
             }
         },
         Commands::RemoteInstall => {
+            remote_install();
         },
     }
 }
-/*
+
 pub fn remote_install() {
+    ssh_ping(&select_host(tailscale_fetch()));
 }
-*/
+
 pub fn tailscale_fetch() -> Taildevices {
     let tail_status = Command::new("tailscale")
         .arg("status")
         .arg("--json")
         .output()
         .expect("Konnte den Befehl: tailscale status --json nicht ausführen");
-    if !tail_status.status.success() {
-        panic!("[ FAILED ] - Tailscale Status ist Fehlgeschlagen, bist du eingelogt, wurde das JSON nicht richtig geparst, ...");
+    if tail_status.status.success() {
+        info!("[ OK ] - Fetched Tailscale Devices");
+    } else if !tail_status.status.success() {
+        error!("[ FAILED ] - Tailscale Status ist Fehlgeschlagen, bist du eingelogt, wurde das JSON nicht richtig geparst, ...");
+        panic!("Abbruch");
     }
     serde_json::from_slice::<Taildevices>(&tail_status.stdout)
         .expect("Konnte das Tailscale-JSON nicht parsen")
 }
 
-pub fn select_host(hosts: Taildevices) {
+pub fn select_host(hosts: Taildevices) -> String {
     let mut options: Vec<String> = vec![];
+    let mut output_ip: String = String::from("127.0.0.1");
     for (_pubkey, device_info) in hosts.devices {
         let ip: &str = device_info.ip.first().map(|s| s.as_str()).unwrap_or("Keine IP");
-        let input = format!("IP: {} - Name: {}", ip, device_info.name);
+        let input = format!("IP: {:<15} - Name: {}", ip, device_info.name);
         options.push(input);
     }
     let answer = Select::new("Select Hosts", options).prompt();
     match answer {
         Ok(choice) => {
-            println!("Deine Auswahl: {}", choice);
             if let Some((ip, _name)) = choice.split_once(" - Name: ") {
                 let clean_ip: &str = ip.strip_prefix("IP: ").unwrap_or(ip).trim();
-                println!("IP: {}", clean_ip);
+                output_ip = String::from(clean_ip);
             }
         },
         Err(e) => {
-            panic!("[ FAILED ] - Konnte den Input nicht auslesen: {}", e);
+            error!("[ FAILED ] - Konnte den Input nicht auslesen: {}", e);
+            panic!("Abbruch");
         }
     }
+    output_ip 
 }
--- xanterella/src/ssh.rs
+++ xanterella/src/ssh.rs
@@ -1,5 +1,7 @@
 use std::process::Command;
 
+use log::{debug, info, error};
+
 pub fn ssh_ping(ip: &String) {
     let ping = Command::new("ping")
         .args(["-c", "1"])
@@ -7,19 +9,41 @@ pub fn ssh_ping(ip: &String) {
         .arg(ip)
         .output()
         .expect("Konnte den Ping nicht starten");
-    if ping.status.success() {
-        println!("[ OK ] - Ping erfolgreich");
-    } else {
-        panic!("[ FAILED ] - Konnte das Gerät nicht pingen: {}", ip);
+
+    if !ping.status.success() {
+        error!("[ FAILED ] - Konnte das Gerät nicht pingen: {}", ip);
+        panic!("Abbruch");
     }
+
+    info!("[ OK ] - Ping erfolgreich");
     let ssh_command = format!("root@{}", ip);
     let ssh = Command::new("ssh")
         .arg(&ssh_command)
         .output()
         .expect("Konnte den SSH nicht starten");
-    if ssh.status.success() {
-        println!("[ OK ] - SSH erfolgreich");
-    } else {
-        panic!("[ FAILED ] - Konnte das Gerät nicht sshen: {}", ssh_command);
+    if !ssh.status.success() {
+        error!("[ FAILED ] - Konnte das Gerät nicht sshen: {}", ssh_command);
+        panic!("Abbruch");
     }
+    info!("[ OK ] - SSH erfolgreich");
+}
+
+pub fn ssh_get_hardware(ip: &String) -> String {
+    let ssh_command = format!("root@{}", ip);
+    let ssh = Command::new("ssh")
+        .arg(&ssh_command)
+        .arg("nixos-generate-config --show-hardware-config")
+        .output()
+        .expect("[ FAILED ] - Konnte die Hardware Config nicht erstellen lassen");
+
+    if !ssh.status.success() {
+        let err = String::from_utf8_lossy(&ssh.stderr);
+        error!("[ FAILED ] - Fehler beim erstellen der Hardware Config: {}", err);
+        panic!("Abbruch");
+    }
+
+    let hardware_config = String::from_utf8_lossy(&ssh.stdout).to_string();
+    info!("[ OK ] - Hardware Config erstellt");
+    debug!("{}", hardware_config);
+    hardware_config
 }
